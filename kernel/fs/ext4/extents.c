f8c3b9445922bc2383841932b855da4b624ae75e
RaNd <rand@uns.gr>
2014-05-20 12:44:55 +0300

    ext4: speed up truncate/unlink by not using bforget() unless needed  (@kashifmin)

--- kernel/fs/ext4/extents.c
+++ kernel/fs/ext4/extents.c
@@ -2208,8 +2208,8 @@ static int ext4_remove_blocks(handle_t *handle, struct inode *inode,
 	struct ext4_sb_info *sbi = EXT4_SB(inode->i_sb);
 	unsigned short ee_len =  ext4_ext_get_actual_len(ex);
 	ext4_fsblk_t pblk;
-	int flags = EXT4_FREE_BLOCKS_FORGET;
-
+	
+	int flags = 0;
 	if (S_ISDIR(inode->i_mode) || S_ISLNK(inode->i_mode))
 		flags |= EXT4_FREE_BLOCKS_METADATA;
 	/*
@@ -2460,8 +2460,11 @@ ext4_ext_rm_leaf(handle_t *handle, struct inode *inode,
 		int flags = EXT4_FREE_BLOCKS_FORGET;
 
 		if (S_ISDIR(inode->i_mode) || S_ISLNK(inode->i_mode))
-			flags |= EXT4_FREE_BLOCKS_METADATA;
+			flags |= EXT4_FREE_BLOCKS_METADATA | EXT4_FREE_BLOCKS_FORGET;
 
+		else if (ext4_should_journal_data(inode))
+
+			flags |= EXT4_FREE_BLOCKS_FORGET;
 		ext4_free_blocks(handle, inode, NULL,
 				 EXT4_C2B(sbi, *partial_cluster),
 				 sbi->s_cluster_ratio, flags);
